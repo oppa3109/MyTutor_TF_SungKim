9b637680e3260bb1423f6064a67de284822992dc
byoungki.jeon <byoungki.jeon@gmail.com>
2017-02-01 17:49:56 +0900

    linear_regression/01.py & 02.py

--- Linear_Regression/02.py
+++ Linear_Regression/02.py
@@ -6,11 +6,14 @@ y_data = [1, 2, 3]
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
 # Hypothesis
-hypothesis = W * x_data + b
+hypothesis = W * X + b
 
 # Cost function
-cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 # Minimize
 a = tf.Variable(0.1)  # learning rate
@@ -26,6 +29,10 @@ sess.run(init)
 
 # Fit the line
 for step in xrange(2001):
-    sess.run(train)
+    sess.run(train, feed_dict={X:x_data, Y:y_data})
     if step % 20 == 0:
-        print step, sess.run(cost), sess.run(W), sess.run(b)
+        print step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W), sess.run(b)
+
+# Inference
+print sess.run(hypothesis, feed_dict={X:5})
+print sess.run(hypothesis, feed_dict={X:2.5})
