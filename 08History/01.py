1a9ff024a69de7360e33cd7a3a4023b016dbdcae
byoungki.jeon <byoungki.jeon@gmail.com>
2017-02-27 18:55:34 +0900

    .

---
+++ 08History/01.py
@@ -0,0 +1,8 @@
+# Perceptron: Frank Rosenblatt(~1957) - AND/OR, impossible to solve XOR
+# MLP: Marvin Minsky(1969) - no method to train NLP >>> dark age
+# Backpropagation: Paul Werbos(1974, 1982), Jeoffrey Hinton(1986) >>> 1st breakthrough
+# hard to train deep MLPs: Yann LeCun(1995) - SVM, Random Forest better than MLP >>> dark age
+# DBN by a good initialization method(layerwise training etc): Jeoffrey Hinton(2006) >>> 2nd breakthrough
+#  - Deep MLPs by a good initialization method: Yoshua Bengio(2007)
+#  - Rebranding of NN to Deep learning
+# Alex Net(2012)
