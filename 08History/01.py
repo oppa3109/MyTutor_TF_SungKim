8eb8d1342e04ab7983a5eb37a6c89d335c12fb8a
byoungki.jeon <byoungki.jeon@gmail.com>
2017-02-28 11:26:28 +0900

    .

--- 08History/01.py
+++ 08History/01.py
@@ -2,7 +2,8 @@
 # MLP: Marvin Minsky(1969) - no method to train NLP >>> dark age
 # Backpropagation: Paul Werbos(1974, 1982), Jeoffrey Hinton(1986) >>> 1st breakthrough
 # hard to train deep MLPs: Yann LeCun(1995) - SVM, Random Forest better than MLP >>> dark age
+# * LeNet-5(CNN): Yann LeCun(1980)
 # DBN by a good initialization method(layerwise training etc): Jeoffrey Hinton(2006) >>> 2nd breakthrough
-#  - Deep MLPs by a good initialization method: Yoshua Bengio(2007)
-#  - Rebranding of NN to Deep learning
+#  * Deep Networks by a good initialization method: Yoshua Bengio(2007)
+#  * Rebranding of NNs to Deep Neural Networks & Deep Learning
 # Alex Net(2012)
