6017b475448a16048daa32803bebca50ac6406e8
byoungki.jeon <byoungki.jeon@gmail.com>
2017-03-28 17:14:27 +0900

    .

--- 03Linear_Regression_GD/02.py
+++ 03Linear_Regression_GD/02.py
@@ -12,17 +12,26 @@ hypothesis = W * X
 
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
+learning_rate = 0.1
+
 # Minimize
 # W := W - a/m * sum{(H(xi) - yi) * xi}
-descent = W - tf.mul(0.1, tf.reduce_min(tf.mul((hypothesis - Y), X)))
-update = W.assign(descent)
+#1 descent = W - tf.mul(0.1, tf.reduce_min(tf.mul((hypothesis - Y), X)))
 
-init = tf.initialize_all_variables()
+#2 descent = W - learning_rate * tf.reduce_mean((hypothesis - Y) * X)
+#2 update = W.assign(descent)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+train = optimizer.minimize(cost)
+
+#init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
 
 for step in xrange(100):
-    sess.run(update, feed_dict={X:x_data, Y:y_data})
+#2    sess.run(update, feed_dict={X:x_data, Y:y_data})
+    sess.run(train, feed_dict={X:x_data, Y:y_data})
     print step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W)
 
