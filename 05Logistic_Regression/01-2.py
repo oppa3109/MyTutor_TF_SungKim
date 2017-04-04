f8b90c0d3d3c27bb21d42ec04c3df38156868cc6
byoungki.jeon <byoungki.jeon@gmail.com>
2017-04-04 16:34:48 +0900

    .

---
+++ 05Logistic_Regression/01-2.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy as np
+
+xy = np.loadtxt('data-03-diabetes.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
+
+X = tf.placeholder(tf.float32, shape=[None, 8])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+W = tf.Variable(tf.random_normal([8, 1], name='weight'))
+b = tf.Variable(tf.random_normal([1], name='bias'))
+
+# Our hypothesis                        H(X) = 1 / (1 + exp(-W^T * X))
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+
+# Cost function                         cost(W) = ...
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+
+# Minimize                           W := W - a * gradient(cost)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+# Accuracy computation
+# True if hypothesis > 0.5 else False
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+# Launch graph
+with tf.Session() as sess:
+    # Initialize Tensorflow variables
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        cost_val, _ = sess.run([cost, train], feed_dict={X:x_data, Y:y_data})
+        if step % 200 == 0:
+            print step, cost_val
+
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X:x_data, Y:y_data})
+    print "\nHypothesis: \n", h, "\nCorrect(Y): \n", c, "\nAccuracy: ", a
